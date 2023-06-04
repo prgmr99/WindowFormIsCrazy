4557f0713277d6b609c1c6c671f2530c03592e3b
prgmr99 <johnyeom24@naver.com>
2023-06-05 02:51:09 +0900

    [Mod] 이식완료 issue #43

--- ConvertGPT/ConvertGPT/Presentation/MainScene/ResultScreen.cs
+++ ConvertGPT/ConvertGPT/Presentation/MainScene/ResultScreen.cs
@@ -37,36 +37,6 @@ namespace ConvertGPT.MainScene
 
         public void dataBind(object obj)
         {
-<<<<<<< HEAD
-            inputText = text;
-            setScreen();
-        }
-
-        private void setScreen() {
-            var text = inputText;
-            var toLanguageItem = "Swift";
-            var fromLanguage = "Python";
-
-            string localConfig = Secret.LocalHost;
-            string exConfig = Secret.ExConnect;
-
-            try
-            {
-                if (text == null || text == "")
-                {
-                    throw new ConvertGPTException(ErrorCode.EmptyInput);
-                }
-                if (toLanguageItem == null)
-                {
-                    throw new ConvertGPTException(ErrorCode.ToLanguageDeselected);
-                }
-                requestConvertAPI(fromLanguage, toLanguageItem.ToString(), text);
-            }
-            catch (ConvertGPTException ex)
-            {
-                Console.WriteLine($"Error: {ex.ErrorMessage}, ErrorCode: {ex.ErrorCode}");
-            }
-=======
             Console.WriteLine("dataBind");
             if (obj is ConvertRequest)
             {
@@ -78,31 +48,13 @@ namespace ConvertGPT.MainScene
         }
 
         private void updateUI() {
-
             this.languageLabel.Text = data.toLanguage;
->>>>>>> main
         }
         private async void requestConvertAPI(ConvertRequest data)
         {
-<<<<<<< HEAD
             string localConfig = Secret.LocalHost;
             string exConfig = Secret.ExConnect;
 
-            GPTTest convertAPI = new GPTTest();
-            Task<string> result = convertAPI.gpttestAsync(from_lang, to_lang, code);
-            Console.WriteLine("서버통신을 시작합니다");
-            await result;
-            Console.WriteLine("서버 응답이 왔습니다.");
-
-            using(MySqlConnection conn = new MySqlConnection(localConfig))
-            {
-                conn.Open();
-                string sql = string.Format("INSERT INTO history (FromLang, ToLang, codeRecord, codeResult) VALUES ('{0}', {1}, {2}, {3});", "\"" + from_lang + "\"", "\"" + to_lang + "\"", code, result.Result);
-
-                MySqlCommand command = new MySqlCommand(sql, conn);
-                command.ExecuteNonQuery();
-            }
-=======
             Console.WriteLine("requestConvertAPI");
             PromptService ps = new PromptService();
             Task<string> result = ps.GetResponse(new ConvertType(data));
@@ -110,8 +62,21 @@ namespace ConvertGPT.MainScene
             Console.WriteLine("서버통신을 시작합니다");
             await result;
             Console.WriteLine($"서버 응답이 왔습니다. \n {result.Result}");
->>>>>>> main
+            using (MySqlConnection conn = new MySqlConnection(localConfig))
+            {
+                conn.Open();
+                string sql = string.Format("INSERT INTO history(FromLang, ToLang, codeRecord, codeResult) VALUES ({0}, {1}, {2}, {3});", "\'" + data.fromLanguage + "\'", "\'" + data.toLanguage + "\'", "\'" + data.code + "\'", "\'" + result.Result + "\'");
 
+                try
+                {
+                    MySqlCommand command = new MySqlCommand(sql, conn);
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
             metroTextBox1.Text = result.Result;
             return;
         }
